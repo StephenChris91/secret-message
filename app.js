ceab72e7a4e7852776591a1c4f435309fd246d55
Stephen Chris <stephenchris91@gmail.com>
2021-04-30 02:52:28 -0700

    .

--- app.js
+++ app.js
@@ -3,12 +3,12 @@ const { hash } = window.location;
  const message = (atob(hash.replace('#', '')))
 
  document.querySelector('.header-text').textContent = 'Send Your Secret Message';
-
+ 
  if(message){
      document.querySelector('#message-form').classList.add('hide');
      document.querySelector('#message-show').classList.remove('hide');
     
-     document.querySelector('h1').innerHTML = message;
+     document.querySelector('h3').innerHTML = message;
      document.querySelector('.header-text').textContent = 'Here is your Secret Message';
  }
 
@@ -20,7 +20,6 @@ document.querySelector('form').addEventListener('submit', event =>{
     document.querySelector('#link-form').classList.remove('hide');
 
     const input = document.querySelector('#message-input');
-    
 
         const encrypted = btoa(input.value)
 
